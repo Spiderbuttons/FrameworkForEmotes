55aff04ff54acd38ba57652abf48d65241e16519
Spiderbuttons <azurylldmm@gmail.com>
2025-11-20 23:01:51 -0500

    Data model for an emote.

---
+++ EmotionData.cs
@@ -0,0 +1,16 @@
+﻿namespace FrameworkedEmotionsMod;
+
+public class EmotionData
+{
+    public string Id { get; set; }
+    public string Texture { get; set; }
+    public int SpriteIndex { get; set; }
+    public int FrameWidth { get; set; } = 16;
+    public int FrameHeight { get; set; } = 16;
+    public int MillisecondsPerFrame { get; set; }
+    public int Loops { get; set; } = 1;
+    public int PositionOffset { get; set; }
+    public string? OpeningTexture { get; set; }
+    public bool ShowOpeningAnimation { get; set; } = true;
+    public bool ShowClosingAnimation { get; set; } = true;
+}
