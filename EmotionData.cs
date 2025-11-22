90543ca1251050dc7ead1b136a606835967b5eff
Spiderbuttons <azurylldmm@gmail.com>
2025-11-21 22:13:09 -0500

    Frame Width/Height are nullable now so that height can default to width and width can default to 16.

--- EmotionData.cs
+++ EmotionData.cs
@@ -5,8 +5,8 @@ public class EmotionData
     public string Id { get; set; }
     public string Texture { get; set; }
     public int SpriteIndex { get; set; }
-    public int FrameWidth { get; set; } = 16;
-    public int FrameHeight { get; set; } = 16;
+    public int? FrameWidth { get; set; }
+    public int? FrameHeight { get; set; }
     public int MillisecondsPerFrame { get; set; }
     public int Loops { get; set; } = 1;
     public int PositionOffset { get; set; }
