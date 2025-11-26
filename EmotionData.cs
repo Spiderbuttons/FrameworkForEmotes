e18a360dfed90a7392cc01d76023d45a36c71202
Spiderbuttons <azurylldmm@gmail.com>
2025-11-26 18:08:23 -0500

    Forgot to add the defaults for MillisecondsPerFrame and PositionOffset.

--- EmotionData.cs
+++ EmotionData.cs
@@ -9,9 +9,9 @@ public class EmotionData
     public int SpriteIndex { get; set; }
     public int? FrameWidth { get; set; }
     public int? FrameHeight { get; set; }
-    public int MillisecondsPerFrame { get; set; }
+    public int MillisecondsPerFrame { get; set; } = 250;
     public int Loops { get; set; } = 1;
-    public Vector2 PositionOffset { get; set; }
+    public Vector2 PositionOffset { get; set; } = Vector2.Zero;
     public string? OpeningTexture { get; set; }
     public bool ShowOpeningAnimation { get; set; } = true;
     public bool ShowClosingAnimation { get; set; } = true;
